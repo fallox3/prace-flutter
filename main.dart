8a8c3c60f89128e67dcb92e5ccf066674bf7f273
fallox3 <128169011+fallox3@users.noreply.github.com>
2023-11-09 16:13:18 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:test1/GradientContainer.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(body: GradientContainer()),
+    ),
+  );
+}
